fe6084c5691306a4c010740291c142dddc4e5712
Olivier Colas <olivercolas.oc@gmail.com>
2022-03-06 15:57:09 +0100

    refactor: removed main folder

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -5,7 +5,7 @@ contract HelloWorld {
     string public name = "Lioloc";
 
     function print() external view returns (string memory) {
-        return string(abi.encodePacked('Hello', name));
+        return string(abi.encodePacked('Hello ', name));
     }
 
     function updateName(string memory _name) external {
